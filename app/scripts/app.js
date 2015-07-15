1daf7ca9dee77d2325970f268bb9228a0f8fdcd4
Xetoris <cmccord4pa@gmail.com>
2015-07-15 00:54:16 -0400

    Cleaned up the javascript a bit. More to come there later.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,56 +1,26 @@
-var matchesTableRendered = false;
-(function(){
+$(document).ready(function(){
   var app = document.querySelector('#page-collection');
-  var matchesTable = document.querySelector('#matches-table');
-  var myFirebaseRef = new Firebase("https://shining-torch-9613.firebaseio.com/");
+
+  //ToDo: Remove this when we figure out timing.
   var userData = [];
-  var matchData = [];
+  var myFirebaseRef = new Firebase("https://shining-torch-9613.firebaseio.com/");
+  //End remove.
 
-  // populate player list
-  myFirebaseRef.child('players').once('value', function(snapshot){
-    var data = snapshot.val();
-    jQuery.each(data, function(index, user){
-      userData.push({
-        id: user._id.oid,
-        first: user.firstName,
-        last: user.lastName,
-        nick: user.nickname
-      });
-    });
-
-    console.log(userData);
-    displayMatches(userData, matchData, matchesTable);
-  });
-
-  // Some Firebase Binding
-  myFirebaseRef.child('matches').once('value', function(snapshot){
-    var data = snapshot.val();
-    jQuery.each(data, function(index, match){
-      matchData.push({
-          type: match.match_type,
-          game1: match.challenger_game1 + ' - ' + match.defender_game1,
-          game2: match.challenger_game2 + ' - ' + match.defender_game2,
-          game3: match.challenger_game3 + ' - ' + match.defender_game3,
-          challengerId: match.challenger.oid,
-          defenderId: match.defender.oid
-        });
-    });
-
-    console.log(matchData);
-    displayMatches(userData, matchData, matchesTable);
-  });
-
-  // Routes
+  // Setup our route collection.
   page('/matches', matches);
   page('/rules', rules);
   page('/live', live);
   page('/roster', roster);
+
+  // Forward default requests to /rules
   page('/', function(){
     page.redirect('/rules');
   });
 
+  // Enable hashbang mode.
   page({ hashbang: true });
 
+  // Define what happens on each route.
   function matches(){
     app.select("matches");
   }
@@ -67,49 +37,51 @@ var matchesTableRendered = false;
     app.select("roster");
   }
 
+  // Turn on request handling.
   page();
-})();
 
-// Renders the match table if it hasn't been rendered yet.
-function displayMatches(userData, matchData, matchesTable){
-  if(matchData == null || matchData.length <= 0 || userData == null || userData.length <= 0 || matchesTable == null || matchesTableRendered == true)
-  {
-    return;
-  }
-
-  var tableData = [];
-
-  jQuery.each(matchData, function(index, match){
-    var challenger = jQuery.grep(userData, function(user, index){
-      return user.id == match.challengerId;
-    })[0];
-
-    var defender = jQuery.grep(userData, function(user, index){
-      return user.id == match.defenderId;
-    })[0];
-
-    tableData.push({
-      type:match.type,
-      challenger: challenger.first + ' ' + challenger.nick + ' ' + challenger.last,
-      defender: defender.first + ' '  + defender.nick + ' ' + defender.last,
-      game1: match.game1,
-      game2: match.game2,
-      game3: match.game3
+  //ToDo: Replace this piece.
+  // populate player list
+  myFirebaseRef.child('players').once('value', function(snapshot){
+    var data = snapshot.val();
+    jQuery.each(data, function(index, user){
+      userData.push({
+        id: user._id.oid,
+        first: user.firstName,
+        last: user.lastName,
+        nick: user.nickname
+      });
     });
   });
+  //End remove.
 
-  console.log(tableData);
+  // Enable firebase table for matches.
+  $('#matches-table').loadFirebaseTable({
+    connectionString: "https://shining-torch-9613.firebaseio.com/",
+    collectionName: "matches",
+    getDataFunction: function(data){
+      var temp = [];
 
-  matchesTable.data = tableData;
-  matchesTableRendered = true;
-}
+      jQuery.each(data, function(index, match){
+          var challenger = jQuery.grep(userData, function(user, index){
+            return user.id == match.challenger.oid;
+          })[0];
 
+          var defender = jQuery.grep(userData, function(user, index){
+            return user.id == match.defender.oid;
+          })[0];
 
-// listen for when Polymer has finished parsing and is ready to roll
-//window.addEventListener('WebComponentsReady', function() {
+          temp.push({
+            type:match.match_type,
+            challenger: challenger.first + ' ' + challenger.nick + ' ' + challenger.last,
+            defender: defender.first + ' '  + defender.nick + ' ' + defender.last,
+            game1: match.challenger_game1 + ' - ' + match.defender_game1,
+            game2: match.challenger_game2 + ' - ' + match.defender_game2,
+            game3: match.challenger_game3 + ' - ' + match.defender_game3
+          });
+      });
 
-    // remove the `unresolved` attribute and fade in the app
-    // this prevents any FOUC from occuring
-    //document.querySelector('body').removeAttribute('unresolved');
-
-//});
+      return temp;
+    }
+  });
+});
---
+++ app/scripts/firebaseTableBinding.js
@@ -0,0 +1,64 @@
+/*
+* A quick jQuery plugin to handle all the binding and checking for our firebase table binding.
+* Requires a passed in object of settings: connection string, collection name, table selector, data formatting function.
+*/
+(function($) {
+    $.fn.loadFirebaseTable = function(options) {
+      // Check if we have already rendered the table via firebaseTableRendered property.
+      var hasFirebaseProperty = this.hasOwnProperty('firebaseTableRendered');
+
+      if (hasFirebaseProperty && this.firebaseTableRendered)
+      {
+        console.log('Table already rendered. Avoiding second render.');
+        return;
+      }
+
+      // If we don't have that property, add it. Default to false.
+      if(!hasFirebaseProperty){
+        $.extend(this, { firebaseTableRendered: false });
+      }
+
+      // Lets map our options. If we provide defaults, add them in here.
+      var settings = $.extend({
+        connectionString: null,
+        collectionName: null,
+        getDataFunction: null
+      }, options);
+
+      // Make sure we have all our properties.
+      if (settings.connectionString == null) {
+        throw 'A firebase connection string must be provided.'
+      }
+
+      if (settings.collectionName == null) {
+        throw 'The collection name must be provided.'
+      }
+
+      if (settings.getDataFunction == null || !jQuery.isFunction(settings.getDataFunction))
+      {
+        throw 'Data function parameter must be provided and must be a function.'
+      }
+
+      console.log('Starting Plugin Code');
+      // Setup our firebase reference.
+      var myFirebaseRef = new Firebase(settings.connectionString);
+      var $table = this;
+      // Bind to our firebase query.
+      myFirebaseRef.child(settings.collectionName).once('value', function(snapshot){
+        console.log('Plugin callback received');
+        // Grab the snapshot of the data.
+        var data = snapshot.val();
+        console.log('Invoking passed method');
+        // Call our format function to generate our table data array.
+        var dataArray = settings.getDataFunction(data);
+
+        console.log('Data Array:');
+        console.log(dataArray);
+
+        console.log('Setting Table Data');
+        // Set the table data, mark that this table instance has been rendered.
+        $table.get(0).data = dataArray;
+        $table.firebaseTableRendered = true;
+      });
+    };
+}(jQuery));
